1ae21f7c415acf3d1aba8b4924ca3093014b86b1
Robert Newson <rnewson@apache.org>
2015-07-27 13:09:38 +0100

    Handle waiting_for_updates in callback
    COUCHDB-2724

--- src/cassim_metadata_cache.erl
+++ src/cassim_metadata_cache.erl
@@ -162,6 +162,8 @@ listen_for_changes(Since) ->
     fabric:changes(DbName, fun changes_callback/2, Since, Args).
 
 
+changes_callback(waiting_for_updates, Acc) ->
+    {ok, Acc};
 changes_callback(start, Since) ->
     {ok, Since};
 changes_callback({stop, EndSeq}, _) ->
